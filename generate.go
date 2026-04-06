cf9450bb116b00314764deea2449bd4600e1fb3a
Chad Haynes <haynesc@gmail.com>
2026-04-05 20:15:31 -0700

    fix tailwind generation

--- generate.go
+++ generate.go
@@ -4,5 +4,4 @@ package maileroo
 //go:generate templ generate
 
 // Build Tailwind CSS
-//go:generate tailwindcss -c ./tailwind.config.js -o ./static/css/output.css --minify
-//go:generate sh -c "printf '.scrollbar-hide{scrollbar-width:none;-ms-overflow-style:none}.scrollbar-hide::-webkit-scrollbar{display:none}' >> ./static/css/output.css"
+//go:generate tailwindcss -i ./static/css/input.css -c ./tailwind.config.js -o ./static/css/output.css --minify
